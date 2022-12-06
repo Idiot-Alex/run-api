aad267f0518a627ed670f38ec77dbb3dad76afb8
Eleven <idiotalex@163.com>
2022-12-06 16:12:16 +0800

    delete rollup *.js

--- client-js/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { terser } from "rollup-plugin-terser";
-import filesize from "rollup-plugin-filesize";
-
-import baseConfig from "./rollup.config.base";
-
-export default {
-    ...baseConfig,
-    plugins: [...baseConfig.plugins, terser(), filesize()],
-};
