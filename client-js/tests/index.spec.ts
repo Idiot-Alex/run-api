4428430dd66f219ad38b25ed6c4ce31bd3a003c9
Eleven <idiotalex@163.com>
2022-12-08 01:11:38 +0800

    add mocha test config and run test success

---
+++ client-js/tests/index.spec.ts
@@ -0,0 +1,9 @@
+// import runapi from '../lib/cjs'
+import { assert } from 'chai'
+
+describe('测试getResult函数', function() {
+    it ('正常入参', function() {
+        // runapi.init('');
+        assert(true, '函数正常入参执行错误')
+    })
+})
